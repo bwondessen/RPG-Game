24e05e031beb1ccba98ac57705c3b94f775f34fe
bwondessen <bwondessen23@gmail.com>
2024-05-24 00:38:16 -0500

    Update the buttons inner text with go store function

--- script.js
+++ script.js
@@ -23,7 +23,9 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function goStore() {
-    console.log("Going to store.");
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
 }
 
 function goCave() {
