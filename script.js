65d9d449b21f5a87162715463edb9568e17571e2
bwondessen <bwondessen23@gmail.com>
2024-05-26 02:30:05 -0500

    Optimize code

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function buyWeapon() {
         currentWeapon++;
         goldText.innerText = gold;
         let newWeapon = weapons[currentWeapon].name;
-        text.innerText = "You now have a new weapon.";
+        text.innerText = "You now have a " + newWeapon + ".";
     }
 }
 
