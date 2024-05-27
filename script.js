6e0558166d4cb0ce70ec86dccbe19a4ec23e6219
bwondessen <bwondessen23@gmail.com>
2024-05-26 23:26:19 -0500

    Display message when user attempts to sell their only weapon

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ const weapons = [
     { name: 'claw hammer', power: 50 },
     { name: 'sword', power: 100 }
 ];
+
 const locations = [
     {
         name: "town square",
@@ -70,10 +71,6 @@ function goCave() {
     update(locations[2]);
 }
 
-function fightDragon() {
-    console.log("Fighting dragon.");
-}
-
 function buyHealth() {
     if (gold >= 10) {
         gold -= 10;
@@ -112,6 +109,8 @@ function sellWeapon() {
         let currentWeapon = inventory.shift();
         text.innerText = "You sold a " + currentWeapon + ".";
         text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "Don't sell your only weapon!";
     }
 }
 
@@ -121,4 +120,8 @@ function fightSlime() {
 
 function fightBeast() {
 
+}
+
+function fightDragon() {
+    console.log("Fighting dragon.");
 }
