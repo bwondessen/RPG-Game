b0603981eca7f12bd5ffe8c09ae3e7a3fd997928
bwondessen <bwondessen23@gmail.com>
2024-05-27 19:28:28 -0500

    Push random number to numbers array via pick function and display number chosen by user

--- script.js
+++ script.js
@@ -265,5 +265,9 @@ function pickEight() {
 }
 
 function pick(guess) {
-
+    const numbers = [];
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:"
 }
